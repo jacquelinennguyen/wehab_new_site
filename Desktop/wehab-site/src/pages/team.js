0b585fc1f3b59c89c32f169965780e96248af0f8
Jacqueline Nguyen <beuyenj@gmail.com>
2020-10-22 02:56:37 -0700

    Image cropping

--- Desktop/wehab-site/src/pages/team.js
+++ Desktop/wehab-site/src/pages/team.js
@@ -13,44 +13,56 @@ const TeamPage = () => {
                 <Container>
                     <Row>
                         <Col md={3} sm={6} xs={12}>
-                            <img class="reframe" src="../team-imgs/heather-culbertson.jpg" alt="Heather Culbertson"></img>
+                            <img class="reframe" src="https://raw.githubusercontent.com/jacquelinennguyen/wehab_new_site/master/Desktop/wehab-site/src/team-imgs/heather-culbertson.jpg" alt="Heather Culbertson"></img>
                             <h3 className="team-name">Heather Culbertson</h3>
                             <h4 className="team-title">
                                 Assistant Professor of Computer Science at USC
                             </h4>
-                            <p className= "team-desc">
-                                <a href="/">LinkedIn</a>
+                            <p id="team-desc" className="team-desc">
+                                hculbert@usc.edu
+                            </p>
+                            <p className= "team-nav">
+                                <a href="https://sites.usc.edu/culbertson/">Website</a>
                             </p>
                         </Col>
                         <Col md={3} sm={6} xs={12}>
-                            <img class="reframe" src="https://wehab.stanford.edu/wp-content/uploads/2018/10/pardis_miri-640x640.png" alt=""></img>
-                            <h3 className="team-name">Pardis "Paris" Miri</h3>
+                            <img class="reframe" src="https://raw.githubusercontent.com/jacquelinennguyen/wehab_new_site/master/Desktop/wehab-site/src/team-imgs/lawrence-fung.jpg" alt="Lawrence Fung"></img>
+                            <h3 className="team-name">Lawrence Fung</h3>
                             <h4 className="team-title">
-                                Postdoctoral Fellow
+                                Assistant Professor of Psychiatry and Behavioral Sciences
                             </h4>
-                            <p className="team-desc">
-                                <a href="/">LinkedIn</a>
-                                <a href="/">GitHub</a>
+                            <p className="team-desc" id="team-desc">
+                                lkfung@stanford.edu
+                            </p>
+                            <p className="team-nav">
+                                <a href="http://med.stanford.edu/funglab.html">Website</a>
                             </p>                        
                         </Col>
                         <Col md={3} sm={6} xs={12}>
-                            <img class="reframe" src="https://wehab.stanford.edu/wp-content/uploads/2018/10/pardis_miri-640x640.png" alt=""></img>
-                            <h3 className="team-name">Pardis "Paris" Miri</h3>
+                            <img class="reframe" src="https://raw.githubusercontent.com/jacquelinennguyen/wehab_new_site/master/Desktop/wehab-site/src/team-imgs/james-gross.jpg" alt="James Gross"></img>
+                            <h3 className="team-name">James Gross</h3>
                             <h4 className="team-title">
-                                Postdoctoral Fellow
+                                Professor of Psychology
                             </h4>
-                            <p className="team-desc">
-                                <a href="/">LinkedIn</a>
-                                <a href="/">GitHub</a>
+                            <p className="team-desc" id="team-desc">
+                                gross@stanford.edu
+                            </p>
+                            <p className="team-nav">
+                                <a href="https://spl.stanford.edu/">Website</a>
+                                
                             </p>                        
                         </Col>
                         <Col md={3} sm={6} xs={12}>
-                            <img class="reframe" src="https://wehab.stanford.edu/wp-content/uploads/2018/10/pardis_miri-640x640.png" alt=""></img>
-                            <h3 className="team-name">Pardis "Paris" Miri</h3>
+                            <img class="reframe" src="/" alt="Antonio Hardan"></img>
+                            <h3 className="team-name">Antonio Hardan</h3>
                             <h4 className="team-title">
-                                Postdoctoral Fellow
+                                Professor of Psychiatry and Behavioral Sciences
                             </h4>
-                            <p className="team-desc">
+                            <div className="team-desc" id="team-desc">
+                                Administrative Contact:
+                                earchiba@stanford.edu
+                            </div>
+                            <p className="team-nav">
                                 <a href="/">LinkedIn</a>
                                 <a href="/">GitHub</a>
                             </p>                        
@@ -58,19 +70,19 @@ const TeamPage = () => {
                     </Row>
                     <Row>
                         <Col md={3} sm={6} xs={12}>
-                            <img class="reframe" src="https://wehab.stanford.edu/wp-content/uploads/2018/10/pardis_miri-640x640.png" alt=""></img>
-                            <h3 className="team-name">Pardis "Paris" Miri</h3>
+                            <img class="reframe" src="/" alt="Richard Harvey"></img>
+                            <h3 className="team-name">Richard Harvey</h3>
                             <h4 className="team-title">
                                 Postdoctoral Fellow
                             </h4>
+                            
                             <p className="team-desc">
-                                <a href="/">LinkedIn</a>
-                                <a href="/">GitHub</a>
+                                <a href="https://spl.stanford.edu/">Website</a>
                             </p> 
                         </Col>
                         <Col md={3} sm={6} xs={12}>
                             <img class="reframe" src="https://wehab.stanford.edu/wp-content/uploads/2018/10/pardis_miri-640x640.png" alt=""></img>
-                            <h3 className="team-name">Pardis "Paris" Miri</h3>
+                            <h3 className="team-name">Katherine Isbister</h3>
                             <h4 className="team-title">
                                 Postdoctoral Fellow
                             </h4>
@@ -81,7 +93,7 @@ const TeamPage = () => {
                         </Col>
                         <Col md={3} sm={6} xs={12}>
                             <img class="reframe" src="https://wehab.stanford.edu/wp-content/uploads/2018/10/pardis_miri-640x640.png" alt=""></img>
-                            <h3 className="team-name">Pardis "Paris" Miri</h3>
+                            <h3 className="team-name">Keith Marzullo</h3>
                             <h4 className="team-title">
                                 Postdoctoral Fellow
                             </h4>
@@ -92,7 +104,7 @@ const TeamPage = () => {
                         </Col>
                         <Col md={3} sm={6} xs={12}>
                             <img class="reframe" src="https://media-exp1.licdn.com/dms/image/C5603AQHJzCp8P19Qeg/profile-displayphoto-shrink_800_800/0?e=1608768000&v=beta&t=zjWkW91gY0-Qm6DvQGd26jizbGuxKNUnlHD7-bbnDHo" alt=""></img>
-                            <h3 className="team-name">Pardis "Paris" Miri</h3>
+                            <h3 className="team-name">Erik Peper</h3>
                             <h4 className="team-title">
                                 Postdoctoral Fellow
                             </h4>
